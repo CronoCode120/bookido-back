121e862e0c23d69a2ff93bacacb379cf02e030af
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-15 16:44:34 +0200

    moved source code to "src" folder

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import process from 'node:process'
 const app = express()
 const port = process.env.PORT ?? 3000
 
+app.use(express.json())
+
+app.get('/books')
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
