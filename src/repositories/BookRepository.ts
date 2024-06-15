93c623f3298f2a0b399be69fde997598b831c83c
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-15 21:22:31 +0200

    changed API url from BookRepository

--- src/repositories/BookRepository.ts
+++ src/repositories/BookRepository.ts
@@ -1,3 +1,11 @@
-class BookRepository {}
+class BookRepository {
+  getFilteredBooks = async () => {
+    const url = 'https://openlibrary.org/search.json?publisher=alpha+decay'
+    const res = await fetch(url)
+    const data = await res.json()
+
+    return data
+  }
+}
 
 export default BookRepository
