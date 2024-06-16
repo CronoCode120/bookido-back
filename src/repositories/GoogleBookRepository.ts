7a8aa1e7758257b16c35b547893220f98b55e11b
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-16 18:14:38 +0200

    adapted GoogleBookRepository to parent abstract class

--- src/repositories/GoogleBookRepository.ts
+++ src/repositories/GoogleBookRepository.ts
@@ -1,6 +1,13 @@
-class GoogleBookRepository {
-  getBooksByPublisher = async () => {
-    const url = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
+import BookRepository from './BookRepository.ts'
+
+class GoogleBookRepository extends BookRepository {
+  apiUrl = 'https://www.googleapis.com/books/v1/volumes'
+  resFields = []
+
+  getBooksByPublisher = async (publisher: string) => {
+    const url = new URL(this.apiUrl)
+    url.searchParams.set('publisher', publisher)
+
     const res = await fetch(url)
     const data = await res.json()
 
