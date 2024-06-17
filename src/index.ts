6af34f59a8ce9f3d37be2d67ab17707a4eb9796d
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-17 18:56:40 +0200

    switched to OpenLibrary repository

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import BookController from './controllers/BookController.ts'
-import BookRepository from './repositories/OpenLibraryBookRepository.ts'
+import OpenLibraryBookRepository from './repositories/OpenLibraryBookRepository.ts'
 
 const app = express()
 const port = process.env.PORT ?? 3000
 
-const bookController = new BookController(new BookRepository())
+const bookController = new BookController(new OpenLibraryBookRepository())
 
 app.use(express.json())
 
