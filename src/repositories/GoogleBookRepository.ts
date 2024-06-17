e0e54c2ab55e19688c00736fb1aa66785a58558d
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-17 18:57:51 +0200

    added "fiekds" param to getBooksByPublisher method

--- src/repositories/GoogleBookRepository.ts
+++ src/repositories/GoogleBookRepository.ts
@@ -2,11 +2,15 @@ import BookRepository from './BookRepository.ts'
 
 class GoogleBookRepository extends BookRepository {
   apiUrl = 'https://www.googleapis.com/books/v1/volumes'
-  resFields = []
+  resFields = [
+    'items/id',
+    'items/volumeInfo(title,categories,publisher,language)'
+  ]
 
   getBooksByPublisher = async (publisher: string) => {
     const url = new URL(this.apiUrl)
-    url.searchParams.set('publisher', publisher)
+    url.searchParams.set('q', `inpublisher:"${publisher}"`)
+    url.searchParams.set('fields', this.resFields.join(','))
 
     const res = await fetch(url)
     const data = await res.json()
