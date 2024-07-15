eba27d577f7aeb6a000551ac1f590149a964da49
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-15 23:33:36 +0200

    added route for user registration

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express from 'express'
 import BookController from './controllers/BookController.js'
+import UserController from './controllers/UserController.js'
 import OpenLibraryBookRepository from './repositories/book/OpenLibraryBookRepository.js'
+import AuthFirebase from './models/AuthFirebase.js'
+import UserRepositoryFirebase from './repositories/user/UserRepositoryFirebase.js'
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
 const bookController = new BookController(new OpenLibraryBookRepository())
+const userController = new UserController(
+  new AuthFirebase(),
+  new UserRepositoryFirebase()
+)
 
 app.use(express.json())
 
 app.get('/books', bookController.getBooks)
+
+app.post('/users/register', userController.signUp)
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
