de71104d02ee080b365188818c3d2632ed837ceb
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-15 11:11:47 +0200

    created User model with param validation

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,22 @@
-class User {}
+import { UserType, validateUser } from '../schemas/user.js'
+
+class User {
+  readonly id
+  readonly email
+  readonly username
+  readonly password
+
+  static create = ({ id, email, username, password }: UserType) =>
+    new User({ id, email, username, password })
+
+  constructor(userParams: UserType) {
+    const { id, username, email, password } = validateUser(userParams)
+
+    this.id = id
+    this.username = username
+    this.email = email
+    this.password = password
+  }
+}
 
 export default User
