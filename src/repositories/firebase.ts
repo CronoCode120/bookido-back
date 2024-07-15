4efe39a552d900fb699b511e4ac13933f7040558
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-15 09:38:31 +0200

    created firebase configuration file

---
+++ src/repositories/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
+export const auth = getAuth(app)
