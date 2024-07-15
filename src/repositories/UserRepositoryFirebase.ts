e088ebea0001d83f5373d8c7a5636a138fa5c0c8
CronoCode120 <ctaverafernandez@gmail.com>
2024-07-15 10:17:47 +0200

    created 'create' method and added 'db' property

--- src/repositories/UserRepositoryFirebase.ts
+++ src/repositories/UserRepositoryFirebase.ts
@@ -1,3 +1,14 @@
-class UserRepositoryFirebase {}
+import { setDoc, doc, Firestore, getFirestore } from 'firebase/firestore'
+import { app } from './firebase.js'
+
+class UserRepositoryFirebase {
+  db: Firestore
+
+  constructor() {
+    this.db = getFirestore(app)
+  }
+
+  create = async (email: string, password: string) => {}
+}
 
 export default UserRepositoryFirebase
