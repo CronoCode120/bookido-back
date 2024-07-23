d1aa55b10ca73f25f6b12700c87fd37906a794ff
LordVader03 <pelliterogarciaalex@gmail.com>
2024-07-23 12:03:54 +0200

    Login Done

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -2,6 +2,7 @@ abstract class AuthUser {
   abstract auth: unknown
 
   abstract createUser(email: string, password: string): Promise<string>
+  abstract login(email: string, password: string): Promise<string>
 }
 
 export default AuthUser
