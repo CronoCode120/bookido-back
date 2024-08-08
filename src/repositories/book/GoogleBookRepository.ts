ae45ebde08c7029da9bbda59c38bfdcf6a5f0915
CronoCode120 <ctaverafernandez@gmail.com>
2024-08-08 12:29:12 +0200

    added method to retrieve book description in GoogleBookRepository

--- src/repositories/book/GoogleBookRepository.ts
+++ src/repositories/book/GoogleBookRepository.ts
@@ -4,7 +4,7 @@ class GoogleBookRepository extends BookRepository {
   apiUrl = 'https://www.googleapis.com/books/v1/volumes'
   resFields = [
     'items/id',
-    'items/volumeInfo(title,categories,publisher,language)'
+    'items/volumeInfo(title,categories,publisher,language,description)'
   ]
 
   getBooksByPublisher = async (page: string = '1', publisher: string) => {
@@ -26,6 +26,16 @@ class GoogleBookRepository extends BookRepository {
 
     const res = await fetch(url)
     const data = await res.json()
+  }
+
+  getDescriptionByISBN = async (isbn: string) => {
+    const url = new URL(this.apiUrl)
+    url.searchParams.set('q', `isbn:${isbn}`)
+    url.searchParams.set('fields', 'items/volumeInfo(description)')
+
+    const res = await fetch(url)
+    const data = await res.json()
+    return data.items[0].volumeInfo.description as string
   }
 }
 
