6a17b7a7ab6a594d3518822015a2f750d476358c
LordVader03 <pelliterogarciaalex@gmail.com>
2024-09-10 14:10:53 +0200

    Review Setter and Getter Done

---
+++ src/repositories/review/ReviewRepositoryFirebase.ts
@@ -0,0 +1,55 @@
+import { setDoc, doc, Firestore, getFirestore, getDocs, collection, deleteDoc, query, where, DocumentData } from 'firebase/firestore'
+import { app } from '../firebase.js'
+import { Rating } from '../../types.js'
+import { userInfo } from 'os'
+
+class ReviewRepositoryFirebase {
+  db: Firestore
+  collection = 'reviews'
+  collectionUsers = 'users'
+
+  constructor() {
+    this.db = getFirestore(app)
+  }
+
+  addReview = async (userId: string, isbn: string, value: Rating, review: string) => {
+    if (review.length <= 5) {
+      const collectionRef = collection(this.db, this.collection, isbn, 'data')
+      const docRef = doc(collectionRef, userId)
+      await setDoc(docRef, { value, review })
+      const collectionRef2 = collection(this.db, this.collectionUsers, userId, 'shelve')
+      const docRef2 = doc(collectionRef2, isbn)
+      await setDoc(docRef2, { value, review })
+
+      const reviewsUpdated = await this.getReviews(isbn)
+      return reviewsUpdated
+    } else {
+      return 'err'
+    }
+  }
+
+  getReviews = async (isbn: string) => {
+    const reviewCollectionRef = collection(this.db, this.collection, isbn, 'data')
+    const querySnapshot = await getDocs(reviewCollectionRef)
+    const reviews: DocumentData[] = []
+
+    querySnapshot.forEach((doc) => {
+      const data = doc.data()
+      if (data) {     
+        reviews.push({ userId: doc.id, ...data })
+      }
+    })
+    
+    return reviews
+  }
+
+  /*removeBookInTable = async (userId: string, isbn: string) => {
+    const docRef = doc(this.db, this.collection, userId, 'table', isbn)
+    await deleteDoc(docRef)
+
+    const booksUpdated = await this.getBooksInShelf(userId)
+    return booksUpdated
+  }*/
+}
+
+export default ReviewRepositoryFirebase
