6cf759e851f2a019d820c342d743890dcdc63740
CronoCode120 <ctaverafernandez@gmail.com>
2024-10-03 09:58:17 +0200

    handled books with no isbn

--- src/utils/filterByIsbn.ts
+++ src/utils/filterByIsbn.ts
@@ -1,8 +1,9 @@
 const filterByIsbn = (books: any[], isbns: String[]) => {
-    const booksToShow = books.filter(
-        book => book.isbn.every(
-            (isbn: string) => !isbns.includes(isbn)))
-    return booksToShow
+  const booksToShow = books.filter(
+    book =>
+      book.isbn && book.isbn.every((isbn: string) => !isbns.includes(isbn))
+  )
+  return booksToShow
 }
 
 export default filterByIsbn
