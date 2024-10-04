cc64db4f51113fc2ce407b5d8aa403ca2a385649
CronoCode120 <ctaverafernandez@gmail.com>
2024-10-04 10:51:49 +0200

    trimmed editions' titles to match them accurately

--- src/utils/filterRepeatedEditions.ts
+++ src/utils/filterRepeatedEditions.ts
@@ -2,10 +2,11 @@ const filterRepeatedEditions = (editions: any[]): any[] => {
   const seenEditions: any[] = []
 
   return editions.filter(edition => {
+    const title = edition.title.trim()
     const found = seenEditions.find(
       seen =>
-        seen.title === edition.title &&
-        seen.publisher.toLowerCase() === edition.publisher.toLowerCase()
+        seen.title.trim() === title &&
+        seen.publisher[0].toLowerCase() === edition.publisher[0].toLowerCase()
     )
 
     if (!found) {
