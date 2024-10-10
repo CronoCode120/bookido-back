785e831eee3bf546cded0286277ccea4117dcfab
CronoCode120 <ctaverafernandez@gmail.com>
2024-10-10 11:59:04 +0200

    updated abstract class to match repositories

--- src/repositories/book/BookRepository.ts
+++ src/repositories/book/BookRepository.ts
@@ -4,11 +4,17 @@ abstract class BookRepository implements BookRepository {
   abstract apiUrl: string
   abstract resFields: string[]
 
-  abstract getBooksByPublisher(page: string, publisher: string): Promise<any>
+  abstract getBooksByPublisher(
+    page: string | undefined,
+    publisher: string,
+    isbns: string[]
+  ): Promise<any>
 
   abstract getBookByISBN(isbn: string, fields: string | undefined): Promise<any>
 
-  abstract getDescriptionByISBN(isbn: string): Promise<string>
+  abstract getDescriptionByISBN(isbn: string | undefined): Promise<string>
+
+  abstract getBooksByTitle(title: string): Promise<any[]>
 }
 
 export default BookRepository
